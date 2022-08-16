af26ac039d7cdf7ad177d614df76277028334435
Iniee <66095409+Iniee@users.noreply.github.com>
2022-08-16 17:31:00 +0100

    Logout

--- resources/views/layouts/navbars/user_sidebar.blade.php
+++ resources/views/layouts/navbars/user_sidebar.blade.php
@@ -40,6 +40,7 @@
                 <div class="dropdown-menu bg-transparent border-0">
                     <a href="{{ route('users.create') }}" class="dropdown-item">Create User</a>
                     <a href="{{ route('users.index') }}" class="dropdown-item">Manage User</a>
+                    
 
                 </div>
             </div>
@@ -61,43 +62,21 @@
         <input class="form-control bg-white border-1" type="search" placeholder="Search">
     </form> --}}
     <div class="navbar-nav align-items-center ms-auto">
-
-        <div class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                <i class="bi bi-bell-fill" style="color: black !important"></i>
-                <span class="d-none d-lg-inline-flex">Notification</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-                <a href="#" class="dropdown-item">
-                    <h6 class="fw-normal mb-0">Profile updated</h6>
-                    <small>15 minutes ago</small>
-                </a>
-                <hr class="dropdown-divider">
-                <a href="#" class="dropdown-item">
-                    <h6 class="fw-normal mb-0">New user added</h6>
-                    <small>15 minutes ago</small>
-                </a>
-                <hr class="dropdown-divider">
-                <a href="#" class="dropdown-item">
-                    <h6 class="fw-normal mb-0">Password changed</h6>
-                    <small>15 minutes ago</small>
-                </a>
-                <hr class="dropdown-divider">
-                <a href="#" class="dropdown-item text-center">See all notifications</a>
-            </div>
-        </div>
         <div class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                 <img class="rounded-circle me-lg-2" src="{{ asset('images/1.jpg') }}" alt=""
                     style="width: 40px; height: 40px;">
-                <span class="d-none d-lg-inline-flex">USER</span>
+                <span class="d-none d-lg-inline-flex">{{ Auth::user()->name }}</span>
             </a>
+
             <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-                <a href="#" class="dropdown-item">Settings</a>
-                {{-- <form action="{{ route ('logout')}}" method="POST" > --}}
-                <a href="{{ route('login') }}" class="dropdown-item">Log Out</a>
-                {{-- @csrf
-            </form> --}}
+                @if (Auth::check())
+                    <li><i class="fa fa-user"></i> {{ Auth::user()->name }}:</li>
+                    <form id="logout-form" action="{{ url('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                @endif
             </div>
         </div>
     </div>
